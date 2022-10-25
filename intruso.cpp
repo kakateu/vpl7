bfb5df0434c414c825dd3214b7debb2ccd84d74a
Matheus Salles <matt.salles@outlook.com>
2022-10-25 19:36:21 -0300

    primeiros passos

---
+++ intruso.cpp
@@ -0,0 +1,7 @@
+#include "intruso.hpp"
+
+void intruso::set_senha_vazada(std::string vazou){
+    // for(int c = 10; c<16; c++){
+    //     senha[c] 
+    // }
+}
--- intruso.hpp
+++ intruso.hpp
@@ -1,7 +1,11 @@
 #include <string>
+#include <vector>
 
 class Intruso{
-    /*Continue a implementação da classe Intruso*/
+    private:
+        int n;
+        std::vector<std::string> cod;
+        std::vector<std::string> senha;
 
     public:
         void set_senha_vazada(std::string vazou);
