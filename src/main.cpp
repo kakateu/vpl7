2ae82ef982c3b257147b28a82adb71045a8959e6
Joao Gomes <joao.victor214@gmail.com>
2022-10-27 09:49:46 -0300

    Fix no problema de leitura da entrada

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,13 @@ int main() {
 	Intruso intruso;
 
 	std::cin >> num_entradas;
+	std::cin.ignore();
+
+	// 1 7 3 9 0 8 5 6 2 4 B C E A E B
+	// 9 0 7 5 8 4 6 2 3 1 E C C B D A
 
 	for (i = 0; i < num_entradas; i++) {
-		std::cin.ignore();
+
 		std::getline(std::cin, senha);
 
 		std::cout << "Senha digitada: " << senha << "\r\n";
