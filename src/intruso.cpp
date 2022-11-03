c7c8b7660334e2560281781ebfbd64dd726f7d74
Joao Gomes <joao.victor214@gmail.com>
2022-11-03 00:30:26 -0300

    Criando os vetores com as relacoes de letra e numeros

--- src/intruso.cpp
+++ src/intruso.cpp
@@ -10,6 +10,8 @@ void Intruso::set_senha_vazada(std::string vazou) {
 	std::string aux_vazou;
 	std::string aux_numeros;
 	std::string aux_digitos;
+	std::string aux_relacao_letras_numeros;
+	std::vector<std::string> relacao_letras_numeros;
 
 	// Pega entrada e retira os espacos
 	for (uint i = 0; i < vazou.size(); i++) {
@@ -38,6 +40,66 @@ void Intruso::set_senha_vazada(std::string vazou) {
 	};
 
 	// std::cout << aux_digitos << "\r\n";
+
+	// Olha todas as letras da senha digitada e junta com os digitos numa string
+	for (uint i = 0; i < aux_digitos.size(); i++) {
+		char c;
+
+		c = aux_digitos[i];
+
+		// std::cout << "Letra: " << c << "\r\n";
+
+		switch (c) {
+			case 'A':
+				// aux_relacao_letras_numeros = aux_digitos[i] + (char)aux_numeros[0] + (char)aux_numeros[1];
+				aux_relacao_letras_numeros = aux_digitos[i];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[0];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[1];
+				break;
+			case 'B':
+				// aux_relacao_letras_numeros = aux_digitos[i] + (char)aux_numeros[2] + (char)aux_numeros[3];
+				aux_relacao_letras_numeros = aux_digitos[i];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[2];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[3];
+				break;
+			case 'C':
+				// aux_relacao_letras_numeros = aux_digitos[i] + (char)aux_numeros[4] + (char)aux_numeros[5];
+				aux_relacao_letras_numeros = aux_digitos[i];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[4];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[5];
+				break;
+			case 'D':
+				// aux_relacao_letras_numeros = aux_digitos[i] + (char)aux_numeros[6] + (char)aux_numeros[7];
+				aux_relacao_letras_numeros = aux_digitos[i];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[6];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[7];
+				break;
+			case 'E':
+				// aux_relacao_letras_numeros = aux_digitos[i] + (char)aux_numeros[8] + (char)aux_numeros[9];
+				aux_relacao_letras_numeros = aux_digitos[i];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[8];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[9];
+				break;
+			default:
+				std::cout << "Entrou default";
+				break;
+		}
+
+		// std::cout << "String: " << aux_relacao_letras_numeros << "\r\n";
+
+		// Caso a string nao esteja vazia, da um push_back no vetor de strings
+		if (aux_relacao_letras_numeros != "") {
+			relacao_letras_numeros.push_back(aux_relacao_letras_numeros);
+			aux_relacao_letras_numeros = "";
+		} else {
+			std::cout << "Relacao vazia";
+		}
+	}
+
+	// Add o vetor de strings no vetor de vetor de string
+	lista_relacao_letras_numeros.push_back(relacao_letras_numeros);
+
+	// std::cout << "Adicionou uma nova senha vazada a lista \r\n";
 }
 
 std::string Intruso::crack_senha() {
