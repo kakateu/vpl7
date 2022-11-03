307965737cc5b29bc86b68bfad465e770260414e
cauet <cauetcherfan@yahoo.com>
2022-11-03 00:14:03 -0300

    Preparando o HPP do intruso

--- include/intruso.hpp
+++ include/intruso.hpp
@@ -2,19 +2,11 @@
 #include <list>
 #include <map>
 #include <string>
-
-// class SenhaVazada {
-// 	std::string _numeros;
-// 	std::string _digitos;
-
-// 	SenhaVazada(std::string numeros, std::string digitos);
-// };
+#include <vector>
 
 class Intruso {
 	/*Continue a implementação da classe Intruso*/
-	std::list<std::string> lista_senhas_digitadas;
-	// std::multimap<char, char> relacao_letras_numeros;
-	std::list<std::multimap<char, char>> lista_relacao_letras_numeros;
+	std::vector<std::vector<std::string>> lista_relacao_letras_numeros;
 
   public:
 	void set_senha_vazada(std::string vazou);
